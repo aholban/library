19a37d8657fc465c971c40b9f0dd1b18a6351488
aholban <andreeaholban98@gmail.com>
2021-10-14 14:05:42 +0100

    It can find a registered member, if it turns out that the visitor is not registered it will offer the option to register

--- src/com/softwareinstitute/andreeaholban/Main.java
+++ src/com/softwareinstitute/andreeaholban/Main.java
@@ -12,31 +12,42 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         List<Member> members = new ArrayList<Member>();
+        int terminate =0;
+        while(terminate==0){
+            System.out.println("Hello! Are you new here? (yes/no)");
+            String isNew = input.nextLine();
+            if (isNew.equals("yes")) {
+                Member visitor = register(members);
 
-        System.out.println("Hello! Are you new here? (yes/no)");
-        String isNew = input.nextLine();
-        if(isNew.equals("yes")){
-            System.out.println("What is your full name?");
-            String name = input.nextLine();
-            String id = generateID();
-            Member visitor = new Member(name, id);
-            members.add(visitor);
-            System.out.println("Name: " +visitor.getName());
-            System.out.println("Card ID: " + visitor.getCardID() );
-        }
-        else {
-            System.out.println("What is your full name?");
-            String name = input.nextLine();
+            } else {
+                System.out.println("What is your full name?");
+                String name = input.nextLine();
+                System.out.println("What is your card ID?");
+                String id = input.nextLine();
+                Member visitor = findMember(members, name, id);
+                if (visitor == null) {
+                    System.out.println("It seems you are not in our record. Do you wish to register? (yes/no)");
+                    String toRegister = input.nextLine();
+                    if(toRegister.equals("yes")){
+                        visitor = register(members);
+                    }
+
+                }
+            }
+
+            terminate = 1;
 
         }
 
         Book book = new Book("Harry Potter", "JK Rowling", Boolean.TRUE, "available");
 
+
     }
 
-    public static Member findMember(Member[] members, String name){
+
+    public static Member findMember(List<Member> members, String name, String id){
         for(Member visitor : members){
-            if(visitor.getName().equals(name))
+            if(visitor.getName().equals(name) && visitor.getCardID().equals(id))
                 return visitor;
         }
 
@@ -58,5 +69,18 @@ public class Main {
         }
 
         return id;
+    }
+
+    public static Member register(List<Member> members){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your full name?");
+        String name = input.nextLine();
+        String id = generateID();
+        Member visitor = new Member(name, id);
+        members.add(visitor);
+        System.out.println("Name: " + visitor.getName());
+        System.out.println("Card ID: " + visitor.getCardID());
+
+        return visitor;
     }
 }
