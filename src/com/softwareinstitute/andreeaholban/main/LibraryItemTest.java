76c772b6750b2a316c4e769eeb14ba8e95c317d3
aholban <andreeaholban98@gmail.com>
2021-10-15 13:39:17 +0100

    Added more tests to LibraryItemTest

--- src/com/softwareinstitute/andreeaholban/main/LibraryItemTest.java
+++ src/com/softwareinstitute/andreeaholban/main/LibraryItemTest.java
@@ -3,6 +3,8 @@ package com.softwareinstitute.andreeaholban.main;
 
 import org.junit.jupiter.api.Test;
 
+import javax.print.Doc;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
@@ -13,7 +15,10 @@ public class LibraryItemTest {
     void testSetTitle(){
         Book book = new Book("Pride and Prejudice", "Jane Austen", Boolean.TRUE, "available");
         book.setTitle("Emma");
+        Document document = new Document("", "", null, "", Boolean.TRUE, "", "", 1, 1, 0, null);
+        document.setTitle("Automatic summarization");
         assertEquals("Emma", book.getTitle());
+        assertEquals("Automatic summarization", document.getTitle());
     }
 
     @Test
@@ -22,4 +27,41 @@ public class LibraryItemTest {
         book.setPublicationDate(LocalDate.of(2017, 7, 5));
         assertEquals(LocalDate.of(2017, 7, 5), book.getPublicationDate());
     }
+
+    @Test
+    void testSetISBN(){
+        Book book = new Book("Pride and Prejudice", "Jane Austen", Boolean.TRUE, "available");
+        book.setISBN("12345");
+        assertEquals("12345", book.getISBN());
+    }
+
+    @Test
+    void testSetBorrowable(){
+        Book book = new Book("Pride and Prejudice", "Jane Austen", Boolean.TRUE, "available");
+        book.setBorrowable(Boolean.FALSE);
+        assertEquals(Boolean.FALSE, book.getBorrowable());
+    }
+
+    @Test
+    void testSetFormat(){
+        Book book = new Book("Pride and Prejudice", "Jane Austen", Boolean.TRUE, "available");
+        book.setFormat("digital");
+        assertEquals("digital", book.getFormat());
+    }
+
+    @Test
+    void testSetSection(){
+        Book book = new Book("Pride and Prejudice", "Jane Austen", Boolean.TRUE, "available");
+        book.setSection("Romance");
+        assertEquals("Romance", book.getSection());
+    }
+
+    @Test
+    void testSetShelf(){
+        Book book = new Book("Pride and Prejudice", "Jane Austen", Boolean.TRUE, "available");
+        book.setShelf(4);
+        assertEquals(4, book.getShelf());
+    }
+
+
 }
